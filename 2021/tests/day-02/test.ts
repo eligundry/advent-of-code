1f35f43d2fb0fb194af1c95ad6843b93695cf4c7
Eli Gundry <eligundry@gmail.com>
2021-12-02 10:51:09 -0500

    Complete day 2, part 2

--- 2021/tests/day-02/test.ts
+++ 2021/tests/day-02/test.ts
@@ -36,17 +36,41 @@ const dive = (movements: Movement[]): number => {
   return horizontal * depth
 }
 
+const diveWithAim = (movements: Movement[]): number => {
+  let horizontal = 0
+  let depth = 0
+  let aim = 0
+
+  movements.forEach((movement) => {
+    switch (movement.direction) {
+      case 'forward':
+        horizontal += movement.distance
+        depth += aim * movement.distance
+        break
+      case 'down':
+        aim += movement.distance
+        break
+      case 'up':
+        aim -= movement.distance
+        break
+    }
+  })
+
+  return horizontal * depth
+}
+
 describe('Day 2: Dive!', () => {
-  it('should handle the test input', async () => {
-    const movements: Movement[] = [
-      { direction: 'forward', distance: 5 },
-      { direction: 'down', distance: 5 },
-      { direction: 'forward', distance: 8 },
-      { direction: 'up', distance: 3 },
-      { direction: 'down', distance: 8 },
-      { direction: 'forward', distance: 2 },
-    ]
-    expect(dive(movements)).toBe(150)
+  const testMovements: Movement[] = [
+    { direction: 'forward', distance: 5 },
+    { direction: 'down', distance: 5 },
+    { direction: 'forward', distance: 8 },
+    { direction: 'up', distance: 3 },
+    { direction: 'down', distance: 8 },
+    { direction: 'forward', distance: 2 },
+  ]
+
+  it('should handle the test input for part 1', async () => {
+    expect(dive(testMovements)).toBe(150)
   })
 
   it('should find the answer to part 1', async () => {
@@ -54,5 +78,16 @@ describe('Day 2: Dive!', () => {
     const distance = dive(movements)
     console.log(`the answer to part 1 is ${distance}`)
     expect(distance).toBe(1670340)
+  })
+
+  it('should handle the test input for part 2', async () => {
+    expect(diveWithAim(testMovements)).toBe(900)
+  })
+
+  it('should fine the answer for part 2', async () => {
+    const movements = await parseInput()
+    const distance = diveWithAim(movements)
+    console.log(`the answer to parth 2 is ${distance}`)
+    expect(distance).toBe(1954293920)
   })
 })

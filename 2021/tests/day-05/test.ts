2c5d1730ba2026e3d72d716e33049152cf275b69
Eli Gundry <eligundry@gmail.com>
2021-12-07 00:17:59 -0500

    Add answer for day 5, part 2

--- 2021/tests/day-05/test.ts
+++ 2021/tests/day-05/test.ts
@@ -18,7 +18,7 @@ const drawMap = (map: number[][]) =>
 
 const findSumOfMostDangerousLines = (
   lines: Line[],
-  size: number = 10
+  { size = 10, diagonal = false }
 ): number => {
   const map = Array.from({ length: size }, () =>
     Array.from({ length: size }, () => 0)
@@ -46,6 +46,18 @@ const findSumOfMostDangerousLines = (
       for (let x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) {
         map[y1][x]++
       }
+    } else if (diagonal) {
+      const sloppingDown = y1 < y2
+      const sloppingRight = x1 < x2
+
+      for (
+        let y = y1, x = x1;
+        (sloppingDown ? y <= y2 : y >= y2) &&
+        (sloppingRight ? x <= x2 : x >= x2);
+        sloppingDown ? y++ : y--, sloppingRight ? x++ : x--
+      ) {
+        map[y][x]++
+      }
     }
   })
 
@@ -63,56 +75,73 @@ const findSumOfMostDangerousLines = (
 }
 
 describe('Day 5: Hydrothermal Venture', () => {
+  const testLines: Line[] = [
+    [
+      [0, 9],
+      [5, 9],
+    ],
+    [
+      [8, 0],
+      [0, 8],
+    ],
+    [
+      [9, 4],
+      [3, 4],
+    ],
+    [
+      [2, 2],
+      [2, 1],
+    ],
+    [
+      [7, 0],
+      [7, 4],
+    ],
+    [
+      [6, 4],
+      [2, 0],
+    ],
+    [
+      [0, 9],
+      [2, 9],
+    ],
+    [
+      [3, 4],
+      [1, 4],
+    ],
+    [
+      [0, 0],
+      [8, 8],
+    ],
+    [
+      [5, 5],
+      [8, 2],
+    ],
+  ]
+
   it('should handle the test input', async () => {
-    const lines: Line[] = [
-      [
-        [0, 9],
-        [5, 9],
-      ],
-      [
-        [8, 0],
-        [0, 8],
-      ],
-      [
-        [9, 4],
-        [3, 4],
-      ],
-      [
-        [2, 2],
-        [2, 1],
-      ],
-      [
-        [7, 0],
-        [7, 4],
-      ],
-      [
-        [6, 4],
-        [2, 0],
-      ],
-      [
-        [0, 9],
-        [2, 9],
-      ],
-      [
-        [3, 4],
-        [1, 4],
-      ],
-      [
-        [0, 0],
-        [8, 8],
-      ],
-      [
-        [5, 5],
-        [8, 2],
-      ],
-    ]
-    expect(findSumOfMostDangerousLines(lines)).toBe(5)
+    expect(findSumOfMostDangerousLines(testLines, { size: 10 })).toBe(5)
   })
 
   it('should find the most dangerout points for part 1', async () => {
     const lines = await parseInput()
-    const answer = findSumOfMostDangerousLines(lines, 1000)
+    const answer = findSumOfMostDangerousLines(lines, { size: 1000 })
     console.log(`the answer for part 1 is ${answer}`)
-    expect(answer).toBeGreaterThan(7318)
+    expect(answer).toBe(7318)
+  })
+
+  it('should find the correct number for diagonal lines in the test input', async () => {
+    expect(
+      findSumOfMostDangerousLines(testLines, { size: 10, diagonal: true })
+    ).toBe(12)
+  })
+
+  it('should find the answer for part 2', async () => {
+    const lines = await parseInput()
+    const answer = findSumOfMostDangerousLines(lines, {
+      size: 1000,
+      diagonal: true,
+    })
+    console.log(`the answer for part 2 is ${answer}`)
+    expect(answer).toBe(19939)
   })
 })

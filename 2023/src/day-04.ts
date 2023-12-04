9d120126997594fa19c6a3c54ae109072f18020f
Eli Gundry <eligundry@gmail.com>
2023-12-04 12:02:06 -0500

    Complete day 4, pt 2

--- 2023/src/day-04.ts
+++ 2023/src/day-04.ts
@@ -81,3 +81,39 @@ describe('calculateValueOfCards (part 1)', () => {
     expect(answer).toBe(27845)
   })
 })
+
+function calculateCountOfCards(lines: string[]): number {
+  const cards = new Array(lines.length).fill(1)
+
+  for (let i = 0; i < lines.length; i++) {
+    const matches = countNumberOfMatchesOnCard(lines[i])
+
+    for (let j = i + 1; j <= i + matches; j++) {
+      cards[j] += cards[i]
+    }
+
+    // console.log({ cards, matches })
+  }
+
+  return cards.reduce((a, c) => a + c)
+}
+
+describe('calculateCountOfCards (part 2)', () => {
+  it('should handle the example input', () => {
+    const input = [
+      'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
+      'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
+      'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
+      'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83',
+      'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36',
+      'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11',
+    ]
+    expect(calculateCountOfCards(input)).toBe(30)
+  })
+
+  it('should get the answer for part 2', async () => {
+    const input = await readInputIntoLines('day-04')
+    const answer = calculateCountOfCards(input)
+    expect(answer).toBe(9496801)
+  })
+})
